a0d2a5503d368b0c7a137de7742b1fe29fa7541a
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-04-29 04:55:41 -0400

    Refactor ERC20 library, update dependencies and scripts
    The commit refactored the ERC20 library in `src/token/ERC20.sol` to improve error handling and made use of Assembly for balance updates. It also integrated the IERC20Errors interface from OpenZeppelin contracts. Additionally, package dependencies were specified, along with scripts for testing and building the project.

--- src/token/ERC20.sol
+++ src/token/ERC20.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {IERC20Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+
 import {AllowanceMap} from "../primitive/AllowanceMap.sol";
 import {BalanceMap} from "../primitive/BalanceMap.sol";
 
@@ -14,38 +16,12 @@ struct ERC20Storage {
 
 library ERC20Lib {
     /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    /**
      * @dev Indicates an error related to the current `balance` of a `sender`. Used in transfers.
      * @param sender Address whose tokens are being transferred.
      * @param balance Current balance for the interacting account.
      * @param needed Minimum amount required to perform a transfer.
      */
     error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
-
-    /**
-     * @dev Indicates a failure with the token `sender`. Used in transfers.
-     * @param sender Address whose tokens are being transferred.
-     */
-    error ERC20InvalidSender(address sender);
-
-    /**
-     * @dev Indicates a failure with the token `receiver`. Used in transfers.
-     * @param receiver Address to which tokens are being transferred.
-     */
-    error ERC20InvalidReceiver(address receiver);
 
     function balanceOf(ERC20Storage storage self, address account) internal view returns (uint256) {
         return self.balances.get(account);
@@ -55,49 +31,60 @@ library ERC20Lib {
         return self.allowances.get(owner, spender);
     }
 
-    function transfer(ERC20Storage storage self, address to, uint256 value) internal returns (bool) {
+    function transfer(ERC20Storage storage self, address to, uint256 value) internal {
         _transfer(self, msg.sender, to, value);
-        return true;
     }
 
     function _transfer(ERC20Storage storage self, address from, address to, uint256 value) private {
-        if (from == address(0)) {
-            revert ERC20InvalidSender(address(0));
-        }
-        if (to == address(0)) {
-            revert ERC20InvalidReceiver(address(0));
-        }
         _update(self, from, to, value);
     }
 
     function _update(ERC20Storage storage self, address from, address to, uint256 value) private {
-        if (from == address(0)) {
-            // Overflow check required: The rest of the code assumes that totalSupply never overflows
-            self.totalSupply += value;
-        } else {
-            uint256 fromBalance = self.balances.get(from);
-            if (fromBalance < value) {
-                revert ERC20InsufficientBalance(from, fromBalance, value);
-            }
-            unchecked {
-                // Overflow not possible: value <= fromBalance <= totalSupply.
-                self.balances.set(from, fromBalance - value);
-            }
-        }
+        _deductBalance(self, from, value);
+        _increaseBalance(self, to, value);
+    }
 
-        if (to == address(0)) {
-            unchecked {
-                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
-                self.totalSupply -= value;
-            }
-        } else {
-            unchecked {
-                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
-                // TODO: optimize this
-                self.balances.set(to, self.balances.get(to) + value);
-            }
+    function _deductBalance(ERC20Storage storage self, address from, uint256 value) private {
+        uint256 fromSlot = self.balances.slot(from);
+        uint256 fromBalance;
+        assembly {
+            fromBalance := sload(fromSlot)
         }
+        if (fromBalance < value) {
+            revert IERC20Errors.ERC20InsufficientBalance(from, fromBalance, value);
+        }
+        assembly {
+            // Overflow not possible: value <= fromBalance <= totalSupply.
+            sstore(fromSlot, sub(fromBalance, value))
+        }
+    }
 
-        emit Transfer(from, to, value);
+    function _increaseBalance(ERC20Storage storage self, address to, uint256 value) private {
+        uint256 toSlot = self.balances.slot(to);
+        assembly {
+            // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
+            let toBalanceBefore := sload(toSlot)
+            sstore(toSlot, add(toBalanceBefore, value))
+        }
+    }
+
+    function _mint(ERC20Storage storage self, address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert IERC20Errors.ERC20InvalidReceiver(address(0));
+        }
+        // Overflow check required: The rest of the code assumes that totalSupply never overflows
+        self.totalSupply += value;
+        _increaseBalance(self, account, value);
+    }
+
+    function _burn(ERC20Storage storage self, address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert IERC20Errors.ERC20InvalidSender(address(0));
+        }
+        unchecked {
+            // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
+            self.totalSupply -= value;
+        }
+        _deductBalance(self, account, value);
     }
 }
