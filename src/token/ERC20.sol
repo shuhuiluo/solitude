dc19e9f0c63db850b79fb1717a9c7e19e6217307
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-05-03 00:00:08 -0400

    Add new ERC20 token contract
    The new contract has been added in the `src/token/ERC20.sol` file. This contract inherits from the `IERC20` and `IERC20Metadata` interfaces and utilizes the `_storage` of type `ERC20Storage`. It implements all the necessary methods specified in the ERC20 standard.

---
+++ src/token/ERC20.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import {ERC20Storage} from "../primitive/ERC20.sol";
+
+contract ERC20 is IERC20, IERC20Metadata {
+    ERC20Storage internal _storage;
+
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    /// @inheritdoc IERC20Metadata
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    /// @inheritdoc IERC20Metadata
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    /// @inheritdoc IERC20Metadata
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+
+    /// @inheritdoc IERC20
+    function totalSupply() public view virtual returns (uint256) {
+        return _storage.totalSupply;
+    }
+
+    /// @inheritdoc IERC20
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _storage.balanceOf(account);
+    }
+
+    /// @inheritdoc IERC20
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        return _storage.allowance(owner, spender);
+    }
+
+    /// @inheritdoc IERC20
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        _storage.approve(spender, value);
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /// @inheritdoc IERC20
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        _storage.transfer(to, value);
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /// @inheritdoc IERC20
+    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
+        _storage.transferFrom(from, to, value);
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
